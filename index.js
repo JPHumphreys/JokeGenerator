a87d465f0cd4e23530ec28807cd3ac88eb356647
Mike Wilson <mrmwilson@live.co.uk>
2020-08-26 19:25:05 +0100

    added new-joke-text in js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 window.onload = function () {
     let joketext = document.getElementById("joke-text");
+
+    joketext.textContent= "tadaka was balanced on release";
+
+    joketext.id= "new-joke-text";
+
     console.log(joketext);
 }
