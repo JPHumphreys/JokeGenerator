60f074a3c22ab86766513188ce8076170e732350
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2020-08-26 19:44:59 +0100

    added some js to toggle and alter css attributes in html code

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 window.onload = function () {
     let joketext = document.getElementById("joke-text");
 
-    joketext.textContent= "tadaka was balanced on release";
+    joketext.textContent = "tadaka was balanced on release";
 
-    joketext.id= "new-joke-text";
+    joketext.id = "new-joke-text";
 
     console.log(joketext);
+}
+
+function handleItemButton() {
+    console.log("item button");
+
+    const joke = document.getElementById("joke-item");
+
+    //joke.classList.remove("joke");
+    //joke.classList.add("newjoke");
+
+    joke.classList.toggle("newjoke");
+}
+
+function handleTextButton() {
+    console.log("text button");
 }
