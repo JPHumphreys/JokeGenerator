3f13d5284b532e6c52494a9b4dde37f1ff0e5b7d
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2020-08-26 20:20:56 +0100

    added comments to help

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 window.onload = function () {
+
+    // this gets a element with an ID of 'joke-text' and sets it to a variable
     let joketext = document.getElementById("joke-text");
 
+    // this sets the textContent of a HTML element to something
     joketext.textContent = "tadaka was balanced on release";
 
     console.log(joketext);
@@ -9,11 +12,15 @@ window.onload = function () {
 function handleItemButton() {
     console.log("item button");
 
+    // this gets an element with an ID of 'joke-item' and sets it to a variable called 'joke'
     const joke = document.getElementById("joke-item");
 
     //joke.classList.remove("joke");
     //joke.classList.add("newjoke");
 
+    /// Toggle adds or removes a class depending on the current state
+    //          if it has a class called newjoke - it removes it
+    ///         if it doesnt - it adds it
     joke.classList.toggle("newjoke");
 }
 
@@ -21,19 +28,23 @@ function handleTextButton() {
 
     console.log("text button");
 
+    // this gets a HTML element with an ID of 'joke-text' and sets it to a variable called 'joketextelement'
     var joketextelement = document.getElementById("joke-text");
 
     console.log(joketextelement);
 
+    // this created a (Boolean - true or false) depending on if the HTML element contained a class called 'joke-class-one'
     var classString = joketextelement.classList.contains("joke-class-one");
 
     console.log(classString);
 
-    if (classString == true){
+    //this checks whether classString is true or false
+    if (classString == true) {
+        //if it is true - it removes 'joke-class-one' and adds 'joke-class-two'
         joketextelement.classList.remove("joke-class-one");
         joketextelement.classList.add("joke-class-two");
-    }
-    else {
+    } else {
+        //if it is false - it removes 'joke-class-two' and adds 'joke-class-one'
         joketextelement.classList.add("joke-class-one");
         joketextelement.classList.remove("joke-class-two");
     }
